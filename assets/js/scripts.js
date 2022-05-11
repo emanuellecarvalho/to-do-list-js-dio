cd518133e55f30b2e0cee84ab25dbe8aeba93dec
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-05-11 11:37:21 -0300

    alteração de estilo, inclusão de evento de Enter e limpeza do campo de texto.
    teste texto longo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,48 @@
 let contadorCheckBox = 1;
 
-function adicionaCheckbox() {
-    let taskInputValue = document.getElementById('task-input').value;
+function adicionaCheckbox(event) {
+  let taskInputValue = document.getElementById("task-input").value;
 
-    if (taskInputValue !== "") {
-        const checkbox = document.createElement('input');
-        const label = document.createElement('label');
+  if (taskInputValue !== "") {
+    const containerTarefa = document.createElement("div");
+    containerTarefa.setAttribute("class", "task-container");
 
+    const checkbox = document.createElement("input");
+    const label = document.createElement("label");
 
-        checkbox.setAttribute('id', 'check' + contadorCheckBox);
+    checkbox.setAttribute("id", "check" + contadorCheckBox);
 
-        checkbox.setAttribute('type', 'checkbox');
-        document.getElementById('tarefas').appendChild(checkbox);
+    checkbox.setAttribute("type", "checkbox");
+    containerTarefa.appendChild(checkbox);
 
+    label.innerText = taskInputValue;
+    label.setAttribute("for", "check" + contadorCheckBox);
+    label.setAttribute("class", "task-item");
+    containerTarefa.appendChild(label);
 
-        label.innerText = taskInputValue;
-        label.setAttribute('for', 'check' + contadorCheckBox);
-        label.setAttribute('class', 'task-item');
-        document.getElementById('tarefas').appendChild(label);
+    document.getElementById("tarefas").appendChild(containerTarefa);
 
-        contadorCheckBox++;
-    }
+    contadorCheckBox++;
+
+    // limpa o texto do input
+    document.getElementById("task-input").value = "";
+  }
 }
+
+document.getElementById("btn-add").addEventListener("click", adicionaCheckbox);
 
 function removeTasks() {
-    let deleteTask = document.getElementById('tarefas');
-    deleteTask.innerText = "";
+  let deleteTask = document.getElementById("tarefas");
+  deleteTask.innerText = "";
 }
+
+function eventEnter(event) {
+    console.log(event)
+  let key = event.which || event.key;
+  if (key === 13) { // 13 representa a tecla Enter 
+    adicionaCheckbox();
+  }
+}
+
+const inputEnter = document.getElementById("task-input");
+inputEnter.addEventListener("keyup", eventEnter);
